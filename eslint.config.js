febca7781ecce27082b226857adce5647e9c322a
DW-Research <pwake1344@gmail.com>
2025-01-08 23:37:29 +0000

    chore: add eslint.config.js to get actions working

---
+++ eslint.config.js
@@ -0,0 +1,27 @@
+import js from '@eslint/js';
+import tsParser from '@typescript-eslint/parser';
+import tsPlugin from '@typescript-eslint/eslint-plugin';
+
+export default [
+  js.configs.recommended,
+  {
+    files: ['**/*.{js,jsx,ts,tsx}'],
+    ignores: [
+      '.next/**',
+      'node_modules/**',
+      'build/**',
+      'dist/**'
+    ],
+    languageOptions: {
+      parser: tsParser,
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+    },
+    plugins: {
+      '@typescript-eslint': tsPlugin
+    },
+    rules: {
+      ...tsPlugin.configs.recommended.rules
+    }
+  }
+]; 
